c16c3a55e1bcf1c0a1f5d08a19a3727201ecb9f9
Geraldo Castro Zampoli <shaka_vp@yahoo.com.br>
2013-09-25 22:18:11 -0300

    Phase 1 - Ended preliminar version of forms and actions, next add persona fusion formulas.

--- Persona/persona_fusion/views.py
+++ Persona/persona_fusion/views.py
@@ -1,15 +1,23 @@
 from django.template import RequestContext
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.shortcuts import render_to_response, redirect
 
-# from persona_fusion.models import Persona
+from persona_fusion.models import Persona
 from persona_fusion.forms import PersonaFusionForm
 
 
 def index(request):
     form = PersonaFusionForm()
-    return render_to_response('index.html', {'form': form}, RequestContext(request))
+    result = '---'
+    return render_to_response('index.html', {'form': form, 'result': result}, RequestContext(request))
 
 
 def calculate(request):
-    return HttpResponse("Calculando....")
+    if request.method == 'POST':
+        form = PersonaFusionForm(request.POST)
+        if form.is_valid():
+            persona1 = Persona.objects.get(pk=int(request.POST['persona1'])).name
+            persona2 = Persona.objects.get(pk=int(request.POST['persona2'])).name
+            persona3 = Persona.objects.get(pk=int(request.POST['persona3'])).name
+            result = persona1 + persona2 + persona3
+            return render_to_response('index.html', {'form': form, 'result': result}, RequestContext(request))
+    return redirect("/index/")
