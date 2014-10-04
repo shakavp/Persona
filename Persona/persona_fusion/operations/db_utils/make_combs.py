488a83286d8ef574fadd9193e6a3b3a6a7aa7885
Geraldo Castro Zampoli <shaka_vp@yahoo.com.br>
2014-10-04 10:23:31 -0300

    Include hex dict to translate the persona name; Minors refactorations

--- Persona/persona_fusion/operations/db_utils/make_combs.py
+++ Persona/persona_fusion/operations/db_utils/make_combs.py
@@ -27,6 +27,23 @@ def read_json():
     return persona_list
 
 
+def format_hex(integer):
+    return str(hex(integer)).split('x')[1]
+
+
+def make_hex_persona_dict():
+    hex_file = open("hex.txt", "w")
+    json_data = open("fixture.json")
+    data = json.load(json_data)
+    for item in data:
+        hex_file.write(format_hex(item['pk']) + "|")
+        hex_file.write(item['fields']['name'] + "\n")
+        hex_file.write(item['fields']['name'] + "|")
+        hex_file.write(format_hex(item['pk']) + "\n")
+    hex_file.close()
+    json_data.close()
+
+
 def make_combinations():
     combs = open("combs.txt", "w")
     persona_list = read_json()
@@ -35,10 +52,10 @@ def make_combinations():
         persona_2 = persona_list[subset[1]]
         result = fusion_persona(persona_1, persona_2)
         if result[0] != 'NA':
-            combs.write(str(hex(PERSONAS[persona_list[subset[0]][0]])).split('x')[1] + "%")
-            combs.write(str(hex(PERSONAS[persona_list[subset[1]][0]])).split('x')[1] + "%")
+            combs.write(format_hex(PERSONAS[persona_list[subset[0]][0]]) + "%")
+            combs.write(format_hex(PERSONAS[persona_list[subset[1]][0]]) + "%")
             combs.write("-%")
-            combs.write(str(hex(PERSONAS[result[0]])).split('x')[1] + "\n")
+            combs.write(format_hex(PERSONAS[result[0]]) + "\n")
 
     for subset in itertools.permutations(range(1, len(persona_list)), 3):
         persona_1 = persona_list[subset[0]]
@@ -46,12 +63,13 @@ def make_combinations():
         persona_3 = persona_list[subset[2]]
         result = fusion_persona(persona_1, persona_2, persona_3)
         if result[0] != 'NA':
-            combs.write(str(hex(PERSONAS[persona_list[subset[0]][0]])).split('x')[1] + "%")
-            combs.write(str(hex(PERSONAS[persona_list[subset[1]][0]])).split('x')[1] + "%")
-            combs.write(str(hex(PERSONAS[persona_list[subset[2]][0]])).split('x')[1] + "%")
-            combs.write(str(hex(PERSONAS[result[0]])).split('x')[1] + "\n")
+            combs.write(format_hex(PERSONAS[persona_list[subset[0]][0]]) + "%")
+            combs.write(format_hex(PERSONAS[persona_list[subset[1]][0]]) + "%")
+            combs.write(format_hex(PERSONAS[persona_list[subset[2]][0]]) + "%")
+            combs.write(format_hex(PERSONAS[result[0]]) + "\n")
 
     combs.close()
 
 if __name__ == '__main__':
-    make_combinations()
+    #make_combinations()
+    make_hex_persona_dict()
